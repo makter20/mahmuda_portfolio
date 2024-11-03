cff15b6d36d631da69c3d92695c2a3e36068041c
Mahmuda Akter <mahmudasaba@gmail.com>
2024-11-02 23:37:51 -0700

    contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,90 @@
+import { useRef, useState } from "react";
+import "./Contact.css";
+import { motion, useInView } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import ContactIcon from "./ContactIcon";
+
+const variants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Contact = () => {
+  const ref = useRef();
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  //const isInView = useInView(ref, { margin: "-100px" });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_d4oql7e",
+        "template_pgctqpq",
+        formRef.current,
+        "FtwjrHZSrhAY09tBO"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      className="contact"
+      id="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <motion.h1 variants={variants}>Let’s work together</motion.h1>
+        <motion.div className="item" variants={variants}>
+          <h2>Mail</h2>
+          <span>mahmudasaba@gmail.com</span>
+        </motion.div>
+        <motion.div className="item" variants={variants}>
+          <h2>Address</h2>
+          <span>Bay Area, California</span>
+        </motion.div>
+      </motion.div>
+      <div className="formContainer">
+        <motion.form
+          ref={formRef}
+          onSubmit={sendEmail}
+          initial="initial"
+          whileInView={{ opacity: 1 }}
+        >
+          <ContactIcon />
+          <input type="text" required placeholder="Name" name="name" />
+          <input type="email" required placeholder="Email" name="email" />
+          <textarea rows={8} placeholder="Message" name="message" />
+          <button className="submitBtn">Submit</button>
+          {error && "Error"}
+          {success && "Success"}
+        </motion.form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;
